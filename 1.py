4d905fe51b349833654a02679e2a5981edc664dc
Michkail <55208634+Michkail@users.noreply.github.com>
2019-09-21 07:38:00 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,45 @@
+																		# Nama (String)
+class nama():
+    nama = "Name"
+ 
+    def dataNama(self, argument):
+        print(self.nama,'   : Michkail ')
+ 
+nama = nama()
+ 
+nama.dataNama(0);
+
+																		# Umur (Number)
+class umur():
+    umur = "Age"
+ 
+    def dataUmur(self, argument):
+        print(self.umur,'    : 23th ')
+ 
+umur = umur()
+ 
+umur.dataUmur(0);
+
+																		# Alamat (String)
+class alamat():
+    alamat = "Address"
+ 
+    def dataAlamat(self, argument):
+        print(self.alamat,': Jakarta Timur ')
+ 
+alamat = alamat()
+ 
+alamat.dataAlamat(0);
+
+																		# Hobi (Array)
+
+hobi=["Drawing", "Reading", "Writing", "Photograph", "Jogging"]
+
+print ('Hobbies : 1.',hobi[1])
+print ('          2.',hobi[2])
+print ('	  3.',hobi[3])
+print ('	  4.',hobi[4])
+print ('	  5.',hobi[0])
+
+none = '' 
+print(none, 'is_merried adalah', bool(none)) 
